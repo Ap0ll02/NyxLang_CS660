e9c0223f0c99c57d6da6cf83236b860133f28970
Contaldi-Quinn-1103 <Qcontaldi@nevada.unr.edu>
2025-10-05 20:33:44 -0700

    Feat: Variables
    I still have a few bugs to work out. for some reason my local repository is not editing so I have to push then repull

--- gp1/.zig-cache/o/434c058490e04c4f2784b0b9c246168f/cimport.h.d
+++ gp1/.zig-cache/o/434c058490e04c4f2784b0b9c246168f/cimport.h.d
@@ -1,2 +1,3 @@
-cimport.o: .zig-cache/o/434c058490e04c4f2784b0b9c246168f/cimport.h \
+cimport.o: \
+  /home/wraith/Documents/NyxLang/NyxLang_CS660/gp1/.zig-cache/o/434c058490e04c4f2784b0b9c246168f/cimport.h \
   /home/wraith/Documents/NyxLang/NyxLang_CS660/gp1/src/parser.tab.h
