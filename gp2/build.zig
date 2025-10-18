dfdc85b2592c5c965a3ee424adbaa0758e38a3b6
apollo <jackratermann@gmail.com>
2025-10-18 11:44:53 -0700

    fix: Zig build verbose update
    Changing the build system to run verbose bison output

--- gp2/build.zig
+++ gp2/build.zig
@@ -36,10 +36,10 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
-    const bison = b.addSystemCommand(&.{"bison", "-d"});
+    const bison = b.addSystemCommand(&.{"bison", "-d", "-v"});
     bison.addFileArg(b.path("src/c11.y"));
 
-    const flex = b.addSystemCommand(&.{"flex"});
+    const flex = b.addSystemCommand(&.{"flex", "-d"});
     flex.addFileArg(b.path("src/c11.l"));
 
 
