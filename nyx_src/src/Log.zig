b10890a2f861f996e383fc5d9476bf65c4392f76
Ap0ll02 <jackratermann@gmail.com>
2025-10-30 16:55:00 -0700

    Feat: Added params

--- nyx_src/src/Log.zig
+++ nyx_src/src/Log.zig
@@ -3,13 +3,13 @@ const std = @import("std");
 var column: i32 = 0;
 var line: i32 = 0;
 
-fn info() void {
+fn info(this_column: i32, this_line: i32, source: []const u8, hint: []const u8) void {
 
 }
-fn Warn() void {
+fn Warn(this_column: i32, this_line: i32, source: []const u8, hint: []const u8) void {
 
 }
-fn Error() void {
+fn Error(this_column: i32, this_line: i32, source: []const u8, hint: []const u8) void {
 
 }
 fn log(this_column: i32, this_line: i32, source: []const u8, hint: []const u8) void {
