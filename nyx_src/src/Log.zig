566f9845177289f54cfab4cf354503b0ed08f559
Ap0ll02 <jackratermann@gmail.com>
2025-10-30 16:48:13 -0700

    Feat: Log file with basic skeleton

---
+++ nyx_src/src/Log.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+var column: i32 = 0;
+var line: i32 = 0;
+
+fn info() void {
+
+}
+fn Warn() void {
+
+}
+fn Error() void {
+
+}
+fn log(this_column: i32, this_line: i32, source: []const u8, hint: []const u8) void {
+    
+}
