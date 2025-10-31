87d0ab90ba12874dc7e062ae10efb47f5c0da5c5
Ap0ll02 <jackratermann@gmail.com>
2025-10-30 17:09:49 -0700

    Feat: Filling out the log function, type enum

--- nyx_src/src/Log.zig
+++ nyx_src/src/Log.zig
@@ -3,15 +3,27 @@ const std = @import("std");
 var column: i32 = 0;
 var line: i32 = 0;
 
-fn info(this_column: i32, this_line: i32, source: []const u8, hint: []const u8) void {
+const LogType = enum {
+    INFO,
+    WARN,
+    ERROR
+};
+
+fn info(this_column: i32, this_line: i32, source: [*c]const u8, hint: [*c]const u8) void {
+}
+fn Warn(this_column: i32, this_line: i32, source: [*c]const u8, hint: [*c]const u8) void {
 
 }
-fn Warn(this_column: i32, this_line: i32, source: []const u8, hint: []const u8) void {
+fn Error(this_column: i32, this_line: i32, source: [*c]const u8, hint: [*c]const u8) void {
 
 }
-fn Error(this_column: i32, this_line: i32, source: []const u8, hint: []const u8) void {
+fn log(this_column: i32, this_line: i32, source: []const u8, hint: []const u8, l_type: LogType) void {
+    switch (l_type) {
+        .INFO => std.debug.print("\x1b[1;35mNyxLang | Info: \x1b[0m"),
+        .ERROR => std.debug.print("\x1b[1;35mNyxLang | Error: \x1b[0m"),
+        .WARN => std.debug.print("\x1b[1;35mNyxLang | Warning: \x1b[0m"),
+    }
 
-}
-fn log(this_column: i32, this_line: i32, source: []const u8, hint: []const u8) void {
+    std.debug.print("");
     
 }
