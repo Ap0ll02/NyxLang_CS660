c46bde80a44b00b79ee159613b1b95ace21e582c
QuinnContaldi <qcontaldi@unr.edu>
2025-11-08 14:29:33 -0800

    Fix Main
    yeah so now its compiling

--- nyx_src/src/main.zig
+++ nyx_src/src/main.zig
@@ -53,7 +53,7 @@ pub fn main() !void {
         return;
     };
     analyzer.setSymbolTable(symbol_table);
-    const new_root = bi.built_in_types(root.?);
+    const new_root = try bi.built_in_types(parse_alloc, root.?);
     // std.debug.print("\n\n\n \x1b[1;33mPARSE/AST PRINTOUT\x1b[0m Nya Nya Meow Meow\n", .{});
     if (ast.debug_mode) {
         std.debug.print("\n\n\n \x1b[1;33mPARSE/AST PRINTOUT\x1b[0m DEBUG MODE ENABLED\n", .{});
